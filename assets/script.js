0fcb3120b4e56eb716ea6585705c263874bc97b3
Emily McCormick <emilyannmccormick@gmail.com>
2021-01-29 15:14:45 -0600

    endgame function

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
 var startButton = document.getElementById('start-btn')
 var nextButton = document.getElementById('next-btn')
+var submitButton = document.getElementById('submit-btn')
+
 var questionContainerEl = document.getElementById('question-container')
 var questionResult = document.getElementById('results-container')
+var scoreContainerEl = document.getElementById('score-container')
 
 var questionEl = document.getElementById('question')
 var answerButtonsEl = document.getElementById('answer-btns')
+var resultsEl = document.getElementById('results-container')
 var currentQuestionIndex = 0
 
 var timer = 60
@@ -12,7 +16,7 @@ var timerEl = document.querySelector(".timer")
 var gameTimer;
 var score = 0
 
-// FINISH FILLING OUT
+// CAN ADD MORE QUESTIONS LATER
 var questionBank = [
     { 
         question: 'What color is NOT in the American flag?',
@@ -33,18 +37,31 @@ var questionBank = [
             {text: '11', correct: false}
         ]
     },
-    
-    // { q: 'How many days are in a week?', a: },
-    // { q: 'What month is Christmas in?', a: },
-    // { q: 'What year was WI founded?', a: },
+
+    { 
+        question: 'What month is Christmas in?',
+        answers: [
+            {text: 'December', correct: true }, 
+            {text: 'February', correct: false }, 
+            {text: 'March', correct: false }, 
+            {text: 'July', correct: false}
+        ]
+    },
   ]; 
 
 
 // GIVEN I am taking a code quiz WHEN I click the start button THEN a timer starts and I am presented with a question
 startButton.addEventListener('click', startQuiz)
+
+
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++
-    setNextQuestion()
+    if (currentQuestionIndex === questionBank.length) {
+        endGame()
+    }
+    else {
+        setNextQuestion()
+    }
 })
 
 
@@ -62,24 +79,15 @@ function startQuiz() {
 function timerTick() {
     timer--
     timerEl.textContent = timer
+    if (timer === 0) {
+        endGame()
+    }
 }
 
 // WHEN I answer a question THEN I am presented with another question - CLICKING NEXT   
 function setNextQuestion() {
     resetState()
     showQuestion(questionBank[currentQuestionIndex])
-    // var currentQuestion = questionBank[currentQuestionIndex]
-    // var questionEl = document.querySelector(".question")
-    // questionEl.textContent = currentQuestion.question
-    // currentQuestion.answers.forEach(function(answer,i) {
-    //     var answerChoice = document.createElement('button')
-    //     answerChoice.setAttribute('value', answer)
-    //     answerChoice.innerHTML = answer
-    //     var answerChoiceHolder = document.querySelector(".answer-choice-holder")
-    //     answerChoice.onclick = checkResult
-    //     answerChoiceHolder.textContent = ""
-    //     answerChoiceHolder.appendChild(answerChoice)
-    // })
 }
 
 function showQuestion(question) {
@@ -102,10 +110,35 @@ function resetState() {
         answerButtonsEl.removeChild(answerButtonsEl.firstChild)
     }
 }
+
+
 function checkResult(e) {
     var selectedButton = e.target
-    // var correct = selectedButton.dataset.correct
+    var correct = selectedButton.dataset.correct
+    setStatusClass(resultsEl, correct)
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
     nextButton.classList.remove('hide')
+
+
+function setStatusClass() {
+    clearStatusClass(resultsEl)
+    if (correct) {
+        document.getElementById('results-container').innerText = "Correct!"
+    }
+    else {
+        resultsEl.innerText ="Wrong"
+    }
+}
+
+function clearStatusClass(resultsEl) {
+    resultsEl.innerText = ""
+}
+
+
+
+
     
     // if (this.value == questionBank[currentQuestionIndex].correctAnswer) {
     //     console.log(this.value)
@@ -121,6 +154,30 @@ function checkResult(e) {
     // }
 }
 
+function endGame() {
+    questionContainerEl.classList.add('hide')
+    resultsEl.classList.add('hide')
+    scoreContainerEl.classList.remove('hide')
+    score = timer
+}    
+
+submitButton.addEventListener('click', saveHighScores())
+
+function saveHighScores() {
+    let initials = document.getElementById('input').value.trim()
+    var HighScores = JSON.parse(localStorage.getItem("High Scores")) || []
+    let newScore = {
+        score: score,
+        initials: initials
+    }
+HighScores.push(newScore)
+
+localStorage.setItem("High Scores", JSON.stringify(HighScores))
+
+// go to new window with high scores - get the high scores out of local storage & sort based on score .sort(), forEach to put on page
+// window.location
+    
+}
 
 
 
